cf1c5fc30cee52a916f1dc3a473123af019e7a83
srux <samrob.nz@gmail.com>
2020-01-28 18:08:39 +1300

    Improved Map

---
+++ index.js
@@ -0,0 +1,25 @@
+var map = L.map('map');
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(-44.7231187, 169.2427972),
+		L.latLng(-44.7033679, 169.1280656)
+	],
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
